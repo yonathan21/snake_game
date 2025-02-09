7ce87790830a057d02bb3faffe115b56bf8e6cc5
yonathan21 <eldanadereje06@gmail.com>
2025-02-09 19:19:18 +0300

    movethesnakehead

--- script.js
+++ script.js
@@ -2,18 +2,45 @@ const playBoard = document.querySelector(".play-board");
 
 let foodX, foodY;
 let snakeX = 10, snakeY = 13;
+let velocityX = 0, velocityY = 0;
 
 const changeFoodPosition = () => {
   foodX = Math.floor(Math.random() * 30) + 1;
   foodY = Math.floor(Math.random() * 30) + 1;
 }
 
+const changeDirection = (e) => {
+
+  if(e.key === 'ArrowUp'){
+    velocityX = 0;
+    velocityY = -1;
+  }else if(e.key === 'ArrowDown'){
+    velocityX = 0;
+    velocityY = 1;
+  }else if(e.key === 'ArrowLeft'){
+    velocityX = -1;
+    velocityY = 0;
+  }else if(e.key === 'ArrowRight'){
+    velocityX = 1;
+    velocityY = 0;
+  }
+  initGame();
+  
+}
+
 const initGame = () => {
+
   let htmlMarkup = `<div class = "food" style = "grid-area: ${foodY} / ${foodX}"></div>`;
+
+  snakeX += velocityX;
+  snakeY += velocityY;
   htmlMarkup += `<div class = "head" style = "grid-area: ${snakeY} / ${snakeX}"></div>`;
   playBoard.innerHTML = htmlMarkup;
+
 }
 
 changeFoodPosition();
-initGame();
+setInterval(initGame, 125);
+
+document.addEventListener('keydown', changeDirection);
 
