4a1dd4deedcfb42951b4566dd8ddf1a5e9dd855a
yonathan21 <eldanadereje06@gmail.com>
2025-02-09 18:54:51 +0300

    creatingsnakehead

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const playBoard = document.querySelector(".play-board");
 
 let foodX, foodY;
+let snakeX = 10, snakeY = 13;
 
 const changeFoodPosition = () => {
   foodX = Math.floor(Math.random() * 30) + 1;
@@ -9,8 +10,10 @@ const changeFoodPosition = () => {
 
 const initGame = () => {
   let htmlMarkup = `<div class = "food" style = "grid-area: ${foodY} / ${foodX}"></div>`;
+  htmlMarkup += `<div class = "head" style = "grid-area: ${snakeY} / ${snakeX}"></div>`;
   playBoard.innerHTML = htmlMarkup;
 }
+
 changeFoodPosition();
 initGame();
 
