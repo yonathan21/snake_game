c5238126b7e2345bdf5dcd18cfb696205d347d0a
yonathan21 <eldanadereje06@gmail.com>
2025-02-09 20:02:05 +0300

    gameoverwhenthesnaetouchestheborder

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 const playBoard = document.querySelector(".play-board");
 
+let gameOver = false;
 let foodX, foodY;
 let snakeX = 10, snakeY = 13;
 let velocityX = 0, velocityY = 0;
 let snakeBody = [];
+let setIntervalId;
 
 const changeFoodPosition = () => {
   foodX = Math.floor(Math.random() * 30) + 1;
   foodY = Math.floor(Math.random() * 30) + 1;
+}
+
+const hundleGameOver = () => {
+  clearInterval(setIntervalId);
+  alert('Game Over! Press OK to replay...');
+  location.reload();
 }
 
 const changeDirection = (e) => {
@@ -31,22 +39,27 @@ const changeDirection = (e) => {
 
 const initGame = () => {
 
+  if(gameOver) return hundleGameOver();
+
   let htmlMarkup = `<div class = "food" style = "grid-area: ${foodY} / ${foodX}"></div>`;
 
   if(snakeX === foodX && snakeY === foodY){
     changeFoodPosition();
     snakeBody.push([foodX,foodY]);
-    console.log(snakeBody);
   }
 
   for(let i = snakeBody.length -1; i > 0; i--){
-    snakeBody[i] = snakeBody[i-1];
+    snakeBody[i] = snakeBody[ i - 1 ];
   }
+
+  snakeBody[0] = [snakeX, snakeY];
 
   snakeX += velocityX;
   snakeY += velocityY;
 
-  snakeBody[0] = [snakeX, snakeY];
+  if(snakeX <= 0 || snakeX > 30 ||snakeY <= 0 ||snakeX > 30){
+    gameOver = true;
+  }
 
   for(let i = 0; i < snakeBody.length; i++){
     htmlMarkup += `<div class = "head" style = "grid-area: ${snakeBody[i][1]} / ${snakeBody[i][0]}"></div>`;
@@ -57,7 +70,7 @@ const initGame = () => {
 }
 
 changeFoodPosition();
-setInterval(initGame, 125);
+setIntervalId = setInterval(initGame, 125);
 
 document.addEventListener('keydown', changeDirection);
 
