fb8084c63a68e0d20b40fc5be021c95966d19fad
yonathan21 <eldanadereje06@gmail.com>
2025-02-10 00:31:15 +0300

    don'tturnback

--- script.js
+++ script.js
@@ -20,16 +20,16 @@ const hundleGameOver = () => {
 
 const changeDirection = (e) => {
 
-  if(e.key === 'ArrowUp'){
+  if(e.key === 'ArrowUp' && velocityY != 1){
     velocityX = 0;
     velocityY = -1;
-  }else if(e.key === 'ArrowDown'){
+  }else if(e.key === 'ArrowDown' && velocityY != -1){
     velocityX = 0;
     velocityY = 1;
-  }else if(e.key === 'ArrowLeft'){
+  }else if(e.key === 'ArrowLeft' && velocityX != 1){
     velocityX = -1;
     velocityY = 0;
-  }else if(e.key === 'ArrowRight'){
+  }else if(e.key === 'ArrowRight' && velocityX != -1){
     velocityX = 1;
     velocityY = 0;
   }
