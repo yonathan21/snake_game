86f4ede3a84716740399d49d692cfd0ace4e188d
yonathan21 <eldanadereje06@gmail.com>
2025-02-09 18:48:34 +0300

    changefoodpositionrandomly

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 const playBoard = document.querySelector(".play-board");
 
-let foodX = 13, foodY = 10;
+let foodX, foodY;
+
+const changeFoodPosition = () => {
+  foodX = Math.floor(Math.random() * 30) + 1;
+  foodY = Math.floor(Math.random() * 30) + 1;
+}
 
 const initGame = () => {
   let htmlMarkup = `<div class = "food" style = "grid-area: ${foodY} / ${foodX}"></div>`;
   playBoard.innerHTML = htmlMarkup;
 }
-
+changeFoodPosition();
 initGame();
 
